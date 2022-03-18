a3976827ed9918d32b4940b203f2abf9602e58c2
karthissit <karthissit@gmail.com>
2022-03-18 22:55:51 +0530

    ZigZagConversion

---
+++ src/main/java/zigZagConversion/ZigZagConversion.java
@@ -0,0 +1,16 @@
+package zigZagConversion;
+
+/**
+ * Created by KTK on 18-Mar-22.
+ */
+public class ZigZagConversion {
+    public static void main(String[] args) {
+
+    }
+
+    public String convert(String s, int numRows) {
+        
+
+        return "";
+    }
+}
