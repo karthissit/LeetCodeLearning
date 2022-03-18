fe6fecf273a8abc4b34999fb89ce8545a79bef75
karthissit <karthissit@gmail.com>
2022-03-18 23:03:28 +0530

    ZigZagConversion1

--- src/main/java/zigZagConversion/ZigZagConversion.java
+++ src/main/java/zigZagConversion/ZigZagConversion.java
@@ -5,12 +5,24 @@ package zigZagConversion;
  */
 public class ZigZagConversion {
     public static void main(String[] args) {
-
+        ZigZagConversion zigzag = new ZigZagConversion();
+        String inputString = "PAYPALISHIRING";
+        int numRows = 3;
+        String convertedString = zigzag.convert(inputString, numRows);
+        System.out.println(convertedString);
     }
 
     public String convert(String s, int numRows) {
-        
+        char[] chars = s.toCharArray();
+        //calculate the step value
+        int stepValue = 0;
+        for(int i = 0; i<numRows; i++){
+            for(int j = 0; j<=chars.length;j++){
+                if(i==numRows-1){
 
+                }
+            }
+        }
         return "";
     }
 }
